d7924ffab5deb60acfa973ac8c87f67e5c701a27
Görkem Efe Dersin <dersingrkmefe2003@gmail.com>
2024-09-04 01:54:41 +0300

    ng

--- modcom/src/app/app-routing.module.ts
+++ modcom/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'placeholders',loadChildren:
+    ()=>import('./placeholders/placeholders.module').then(m=> m.PlaceholdersModule)
+  },
+  {path:'tables',loadChildren:
+    ()=>import('./tables/tables.module').then(t => t.TablesModule)
+  },
+  {path:"",component:HomeComponent},
+  {path:"",component:HomeComponent},
+  {path:"**",component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- modcom/src/app/app.module.ts
+++ modcom/src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PlaceholdersModule } from './placeholders/placeholders.module'
-import { TablesModule } from './tables/tables.module'
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    PlaceholdersModule,
-    TablesModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
